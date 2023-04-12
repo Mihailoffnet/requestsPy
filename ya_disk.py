fcf97e140ef2e36b76f30a6240c7fc50c8050f4a
Mihailoff <127674824+Mihailoffnet@users.noreply.github.com>
2023-04-12 20:29:30 +0500

    Update ya_disk.py

--- ya_disk.py
+++ ya_disk.py
@@ -11,13 +11,17 @@ class YandexDisk:
     def get_headers(self):
         return {
             'Content-Type': 'application/json',
-            'Authorization': 'OAuth {}'.format(self.token)
+            'Authorization': f'OAuth {self.token}'
         }
 
     def get_files_list(self):
         files_url = 'https://cloud-api.yandex.net/v1/disk/resources/files'
         headers = self.get_headers()
-        response = requests.get(files_url, headers=headers)
+        response = requests.get(url=files_url, headers=headers)
+        # pprint(f'json из гет файлс лист {response.json()} {type(response.json)}')
+        # pprint(f'контект из гет файлс лист {response.content}')
+        print(response.status_code)
+        print()
         return response.json()
 
     def _get_upload_link(self, disk_file_path):
@@ -25,12 +29,14 @@ class YandexDisk:
         headers = self.get_headers()
         params = {"path": disk_file_path, "overwrite": "true"}
         response = requests.get(upload_url, headers=headers, params=params)
-        pprint(response.json())
+        pprint(f'респонс из _get_upload_link {response.json()}')
+        print()
         return response.json()
 
     def upload_file_to_disk(self, disk_file_path, filename):
         href = self._get_upload_link(disk_file_path=disk_file_path).get("href", "")
         response = requests.put(href, data=open(filename, 'rb'))
         response.raise_for_status()
+        print(response.status_code)
         if response.status_code == 201:
             print("Success")
